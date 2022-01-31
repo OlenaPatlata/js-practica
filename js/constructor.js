3de4661b02f5351c17a8ea8892d30b055fd19242
OlenaPatlata <94738131+OlenaPatlata@users.noreply.github.com>
2022-01-31 15:53:06 +0200

    Добавляет исходные файлы

---
+++ js/constructor.js
@@ -0,0 +1,227 @@
+const homeworkResults = [
+	{
+		topic: 'HTML Basics',
+		results: [
+			{
+				email: 'john@gmail.com',
+				success: true,
+			},
+			{
+				email: 'jane@gmail.com',
+				success: true,
+			},
+			{
+				email: 'jack@gmail.com',
+				success: true,
+			},
+			{
+				email: 'mary@gmail.com',
+				success: true,
+			},
+			{
+				email: 'robert@gmail.com',
+				success: true,
+			},
+			{
+				email: 'ann@gmail.com',
+				success: true,
+			},
+			{
+				email: 'brad@gmail.com',
+				success: true,
+			},
+			{
+				email: 'lisa@gmail.com',
+				success: true,
+			},
+			{
+				email: 'matthew@gmail.com',
+				success: true,
+			},
+			{
+				email: 'sandra@gmail.com',
+				success: true,
+			},
+		],
+	},
+	{
+		topic: 'CSS Basics',
+		results: [
+			{
+				email: 'john@gmail.com',
+				success: false,
+			},
+			{
+				email: 'jane@gmail.com',
+				success: true,
+			},
+			{
+				email: 'jack@gmail.com',
+				success: true,
+			},
+			{
+				email: 'mary@gmail.com',
+				success: false,
+			},
+			{
+				email: 'robert@gmail.com',
+				success: true,
+			},
+			{
+				email: 'ann@gmail.com',
+				success: true,
+			},
+			{
+				email: 'brad@gmail.com',
+				success: true,
+			},
+			{
+				email: 'lisa@gmail.com',
+				success: false,
+			},
+			{
+				email: 'matthew@gmail.com',
+				success: true,
+			},
+			{
+				email: 'sandra@gmail.com',
+				success: true,
+			},
+		],
+	},
+	{
+		topic: 'JS Basics',
+		results: [
+			{
+				email: 'john@gmail.com',
+				success: true,
+			},
+			{
+				email: 'jane@gmail.com',
+				success: false,
+			},
+			{
+				email: 'jack@gmail.com',
+				success: true,
+			},
+			{
+				email: 'mary@gmail.com',
+				success: true,
+			},
+			{
+				email: 'robert@gmail.com',
+				success: true,
+			},
+			{
+				email: 'ann@gmail.com',
+				success: false,
+			},
+			{
+				email: 'brad@gmail.com',
+				success: true,
+			},
+			{
+				email: 'lisa@gmail.com',
+				success: true,
+			},
+			{
+				email: 'matthew@gmail.com',
+				success: false,
+			},
+			{
+				email: 'sandra@gmail.com',
+				success: true,
+			},
+		],
+	},
+	{
+		topic: 'DOM',
+		results: [
+			{
+				email: 'john@gmail.com',
+				success: false,
+			},
+			{
+				email: 'jane@gmail.com',
+				success: true,
+			},
+			{
+				email: 'jack@gmail.com',
+				success: true,
+			},
+			{
+				email: 'mary@gmail.com',
+				success: true,
+			},
+			{
+				email: 'robert@gmail.com',
+				success: false,
+			},
+			{
+				email: 'ann@gmail.com',
+				success: true,
+			},
+			{
+				email: 'brad@gmail.com',
+				success: true,
+			},
+			{
+				email: 'lisa@gmail.com',
+				success: true,
+			},
+			{
+				email: 'matthew@gmail.com',
+				success: false,
+			},
+			{
+				email: 'sandra@gmail.com',
+				success: true,
+			},
+		],
+	},
+	{
+		topic: 'AJAX',
+		results: [
+			{
+				email: 'john@gmail.com',
+				success: false,
+			},
+			{
+				email: 'jane@gmail.com',
+				success: true,
+			},
+			{
+				email: 'jack@gmail.com',
+				success: false,
+			},
+			{
+				email: 'mary@gmail.com',
+				success: true,
+			},
+			{
+				email: 'robert@gmail.com',
+				success: true,
+			},
+			{
+				email: 'ann@gmail.com',
+				success: false,
+			},
+			{
+				email: 'brad@gmail.com',
+				success: true,
+			},
+			{
+				email: 'lisa@gmail.com',
+				success: true,
+			},
+			{
+				email: 'matthew@gmail.com',
+				success: true,
+			},
+			{
+				email: 'sandra@gmail.com',
+				success: true,
+			},
+		],
+	},
+];
