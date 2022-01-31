044fcb654456321abffb790abcaede956f5d0013
OlenaPatlata <94738131+OlenaPatlata@users.noreply.github.com>
2022-01-31 17:51:29 +0200

    Решение первой задачи

--- js/constructor.js
+++ js/constructor.js
@@ -1,3 +1,48 @@
+const listOfStudents = [
+	{
+		name: 'John',
+		email: 'john@gmail.com',
+	},
+	{
+		name: 'Jane',
+		email: 'jane@gmail.com',
+	},
+	{
+		name: 'Jack',
+		email: 'jack@gmail.com',
+	},
+	{
+		name: 'Mary',
+		email: 'mary@gmail.com',
+	},
+	{
+		name: 'Robert',
+		email: 'robert@gmail.com',
+	},
+	{
+		name: 'Ann',
+		email: 'ann@gmail.com',
+	},
+	{
+		name: 'Brad',
+		email: 'brad@gmail.com',
+	},
+	{
+		name: 'Lisa',
+		email: 'lisa@gmail.com',
+	},
+	{
+		name: 'Matthew',
+		email: 'matthew@gmail.com',
+	},
+	{
+		name: 'Sandra',
+		email: 'sandra@gmail.com',
+	},
+];
+
+
+
 const homeworkResults = [
 	{
 		topic: 'HTML Basics',
@@ -225,3 +270,49 @@ const homeworkResults = [
 		],
 	},
 ];
+// __________________Задача 1, 31/01/2022
+// 1. Вы должны создать конструктор функций 'Student', который вы можете вызвать с двумя аргументами name и email.
+// Имя, адрес электронной почты и homeworkResult должны быть скрыты от пользователя. 
+// Экземпляр Student должен  предоставить следующие методы для получения или изменения своего состояния:
+// getName(): возвращает имя учащегося.
+// getEmail(): возвращает адрес электронной почты учащегося.
+// addHomeworkResult(тема, успех): этот метод можно вызвать с 2 аргументами: topic(string) и success(boolean). Этот метод должен добавить новый элемент в свойство homeworkResults учащегося.
+
+class Student {
+    #name;
+    #email;
+    #homeworkResults = [];
+    constructor(name, email) {
+        this.#name = name;
+        this.#email = email;
+    }
+    getName() {
+    return console.log(this.#name)
+    }
+    getEmail() {
+    return console.log(this.#email)
+    }
+    addHomeworkResult(topic, success) {
+        this.#homeworkResults.push({ topic, success })
+    }
+    getHomeworkResult() {
+        return console.log(this.#homeworkResults) 
+    }
+}
+const student = new Student('John', 'john@gmail.com') 
+
+student.getName()
+student.getEmail()
+student.addHomeworkResult("HTML", true)
+student.addHomeworkResult("JS", true)
+student.getHomeworkResult()
+
+// __________________Задача 2, 31/01/2022
+// 2. Необходимо создать конструктор функций 'FrontendLab' , который можно вызывать с двумя аргументами students и failedLimit.
+// Экземпляр FrontendLab должен иметь 2 поля свойств: failedHomeworksLimit и studentsList. Эти поля должны быть скрыты от пользователя.
+// Экземпляр Student должен предоставить следующие методы для получения или изменения своего состояния.
+// printStudentsList(): этот метод записывает в консоль список учащихся с результатами их домашних заданий.
+// addHomeworkResults(homeworkResults): этот метод можно вызвать с аргументом homeworkResult, объект с 2 полями: topic(string) и results(массив объектов с 2 полями: email(string) и success(boolean)). Этот метод должен обновить все объекты student в FrontendLab studentsList.
+// printStudentsEligibleForTest(): этот метод должен регистрировать в консоли список студентов, которые не провалили больше домашних заданий, чем разрешено failedLimit. 
+
+
